05a7c0d5f94e14186c5e020faeeccbb7212b22e9
p-oong <lhp7505@naver.com>
2024-07-10 20:43:23 +0900

    Beakjoon/11478/서로다른부분문자열

--- Poong/3week/Baekjoon_11478_서로 다른 부분 문자열의 개수.py
+++ Poong/3week/Baekjoon_11478_서로 다른 부분 문자열의 개수.py
@@ -1,1 +1,27 @@
 # https://www.acmicpc.net/problem/11478
+
+import sys
+input = sys.stdin.read
+
+S = input().strip()
+
+substrings = []
+
+
+for i in range(len(S)):
+    for j in range(i+1, len(S)+1):
+        substrings.append(S[i:j])
+
+
+substrings.sort()
+
+# 중복 제거 및 개수 세기
+unique_count = 0
+prev_substring = ""
+for substring in substrings:
+    if substring != prev_substring:
+        unique_count += 1
+        prev_substring = substring
+
+
+print(unique_count)
